ee927cde1dddef1bbff6e9c1807bf6fc5780f4fa
kaushalachrekar2-rgb <kaushalachrekar2@gmail.com>
2026-01-29 12:15:59 +0530

    Delete data directory

--- data/images.js
+++
@@ -1,8 +0,0 @@
-// images.js - Picture Collection Data
-
-const pictureCollection = Array.from({length: 20}, (_, i) => ({
-    id: i + 1,
-    url: `https://picsum.photos/400/400?random=${i + 100}`,
-    title: `Image ${i + 1}`,
-    category: ['Nature', 'Urban', 'Portrait', 'Abstract'][Math.floor(Math.random() * 4)]
-}));
